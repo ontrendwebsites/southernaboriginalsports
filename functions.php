17fba3c90af4565c37c473dd6f6c66b54ae6b73a
Andrew McDougal <andrew.mcdougal@powershop.com.au>
2021-07-14 23:17:25 +1000

    Add assets to functions file to load on page

--- functions.php
+++ functions.php
@@ -8,16 +8,19 @@ function my_theme_enqueue_styles() {
 function add_files() {
 
   wp_enqueue_script( 'remodal-script', get_stylesheet_directory_uri() . '/library/remodal/remodal.min.js', array(), '' );
+  wp_enqueue_script( 'pushy-script', get_stylesheet_directory_uri() . '/library/js/pushy.js', array(), '', true );
 
 	// custom js
-	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/library/js/custom.js?v=1');
+	wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/library/js/custom.js', array(), '1.0', true);
 
 
   wp_enqueue_style( 'remodal-style', get_stylesheet_directory_uri() . '/library/remodal/remodal.min.css', array(), '' );
   wp_enqueue_style( 'remodal-themestyle', get_stylesheet_directory_uri() . '/library/remodal/remodal-default-theme.min.css', array(), '' );
+  wp_enqueue_style( 'pushy-style', get_stylesheet_directory_uri() . '/library/css/pushy.css', array(), '' );
+  wp_enqueue_style( 'hamburger-style', get_stylesheet_directory_uri() . '/library/css/hamburgers.css', array(), '' );
 
 	// custom stylesheet
-	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/library/css/custom.css?v=4');
+	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/library/css/custom.css?v=5');
 }
 
 add_action( 'wp_enqueue_scripts', 'add_files' );
