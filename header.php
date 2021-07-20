7383401795a2bddbd8244c713a56e96c501401ac
Andrew McDougal <andrew.mcdougal@powershop.com.au>
2021-07-20 22:57:28 +1000

    Header WIP for review

--- header.php
+++ header.php
@@ -82,29 +82,31 @@
             <a href="<?php echo home_url(); ?>" rel="nofollow">
               <?php get_template_part('library/images/inline', 'sas-logo-main.svg'); ?>
             </a>
-            <h4 class="alt-font">
-              <?php //bloginfo('description'); ?>
-              <span>100%</span> Aboriginal owned and Community Grown
-            </h4>
+            <div class="o-flex-layout o-flex-layout--column header-layout">
+
+              <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+              <?php wp_nav_menu(array(
+               'container' => false,                           // remove nav container
+               'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+               'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+               'menu_class' => 'nav top-nav cf',               // adding custom nav class
+               'theme_location' => 'main-nav',                 // where it's located in the theme
+               'before' => '',                                 // before the menu
+                 'after' => '',                                  // after the menu
+                 'link_before' => '',                            // before each link
+                 'link_after' => '',                             // after each link
+                 'depth' => 0,                                   // limit the depth of the nav
+               'fallback_cb' => ''                             // fallback function (if there is one)
+              )); ?>
+              </nav>
+
+              <h4 class="alt-font">
+                <?php //bloginfo('description'); ?>
+                <span>100%</span> Aboriginal owned and Community Grown
+              </h4>
+
+            </div>
           </section>
-
-
-          <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-            <?php wp_nav_menu(array(
-                       'container' => false,                           // remove nav container
-                       'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-                       'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-                       'menu_class' => 'nav top-nav cf',               // adding custom nav class
-                       'theme_location' => 'main-nav',                 // where it's located in the theme
-                       'before' => '',                                 // before the menu
-                             'after' => '',                                  // after the menu
-                             'link_before' => '',                            // before each link
-                             'link_after' => '',                             // after each link
-                             'depth' => 0,                                   // limit the depth of the nav
-                       'fallback_cb' => ''                             // fallback function (if there is one)
-            )); ?>
-
-          </nav>
 
         </div>
 
