2b9968c6ffc0aa4bfd57df79b45b951848786fc3
Andrew McDougal <andrew@ontrendwebsites.com>
2021-07-26 08:18:14 +1000

    WIP new header scroll function

--- library/js/custom.js
+++ library/js/custom.js
@@ -12,23 +12,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
     } 
   });
 
+
   // Sticky header on scroll
+  const mobileHeader = document.querySelector('header.active');
+  var tl = gsap.timeline();
+  tl.to(".header", {x: 100, duration: 1});
+  tl.pause();
+  
+  
   gsap.to('.header', {
     scrollTrigger: {
       trigger: '#content',
-      start: 'top top+=00',
+      start: 'top top+=0',
       endTrigger: '.footer',
       end: 'bottom',
       scrub: true,
       // markers: true
-      //markers: {startColor: "green", endColor: "red", fontSize: "12px"},
-      onToggle: ({isActive}) => console.log('cunce!'),
+      markers: {startColor: "green", endColor: "red", fontSize: "12px"},
+      onEnter: () => tl.play(),
+      onLeaveBack: () => tl.reverse(),
+      //onToggle: self => console.log("toggled. active?", self.isActive),
+
+      //onEnter: () => console.log('onEnter'),
+      onLeave: () => console.log('onLeave'),
+      onEnterBack: () => console.log('onEnterBack'),
+      //onLeaveBack: () => console.log('onLeaveBack'),
+
 
       toggleClass: {targets: ".header", className: "active"}
     } 
   });
 
 });
-
 
 
