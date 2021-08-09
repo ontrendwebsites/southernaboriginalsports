1558a8043373909b754c6606d97669459203d759
Andrew McDoougal <andrew@ontrendwebsites.com>
2021-08-10 08:42:53 +1000

    WIP parallax for review

--- library/js/custom.js
+++ library/js/custom.js
@@ -36,6 +36,25 @@ window.addEventListener('DOMContentLoaded', (event) => {
     toggleClass: {targets: ".header", className: "active"}
   });
 
+
+  // parallax
+  const 
+  bg = document.getElementById("bg");
+  moon = document.getElementById("moon");
+  mountain = document.getElementById("mountain");
+  road = document.getElementById("road");
+  text = document.getElementById("text");
+
+  window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    moon.style.left = -value * 0.5 + 'px';
+    mountain.style.top = -value * 0.15 + 'px';
+    road.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+  })
+
 });
 
 
